909c7696ca78c40b1a1e3a30f1800a2027296e77
Noah Trexler <trexlernoah@gmail.com>
2024-11-08 11:54:27 -0500

    screenshot

--- src/app/flashcards/flashcard-page/flashcard-page.component.ts
+++ src/app/flashcards/flashcard-page/flashcard-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { FlashcardComponent } from '../flashcard/flashcard.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-flashcard-page',
   standalone: true,
-  imports: [FlashcardComponent],
+  imports: [FlashcardComponent, MatButtonModule, MatIconModule],
   templateUrl: './flashcard-page.component.html',
-  styleUrl: './flashcard-page.component.css'
+  styleUrl: './flashcard-page.component.css',
 })
-export class FlashcardPageComponent {
-
-}
+export class FlashcardPageComponent {}
